0f631b7552d9c59fd28c7cb42fd9e238bcf816bf
Dagony <dagony@gmail.com>
2018-02-25 16:10:19 +0100

    Outputting Lists

--- src/App.js
+++ src/App.js
@@ -53,18 +53,11 @@ class App extends Component {
         if (this.state.showPersons) {
             persons = (
                 <div>
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age}/>
-                    <Person
-                        click={this.switchNameHandler.bind(this, 'Max!')}
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        changed={this.nameChangedHandler}
-                    >My Hobbies: Racing</Person>
-                    <Person
-                        name={this.state.persons[2].name}
-                        age={this.state.persons[2].age}/>
+                    {this.state.persons.map(person => {
+                        return (<Person
+                            name={person.name}
+                            age={person.age}/>);
+                    })}
                 </div>
             );
         }
@@ -75,7 +68,7 @@ class App extends Component {
                 <p>This is really working</p>
                 <button
                     style={style}
-                    onClick={this.togglePersonsHandler}>Switch Name
+                    onClick={this.togglePersonsHandler}>Toggle Persons
                 </button>
                 {persons}
 
